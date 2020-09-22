11a70846497a4ae1ccc0b198a855278da4d22a3f
DanaDro <danadrosvit5@gmail.com>
2020-09-22 19:44:24 +0300

    Delete DateUtils.java

--- src/com/johnbryce/project/utils/DateUtils.java
+++
@@ -1,8 +0,0 @@
-package com.johnbryce.project.utils;
-
-public class DateUtils {
-	
-	public static String niceDate(java.sql.Date date) {
-		return String.format("%02d/ %02d/ %04d", date.getDate(), date.getMonth(), date.getYear());
-	}
-}
